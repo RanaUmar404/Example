8691effbd905f0e8e7cce1c863c6eaa822092089
Rana Umar <umarkhan.uk0404@gmail.com>
2022-09-25 02:16:59 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,110 @@
+// let num:number=10
+// console.log(num);
+// // arithmetic operator
+// let a=10
+// let b=5
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+// console.log(a++);
+// console.log(a);
+// console.log(++a);
+// console.log(a);
+// Assignment operator
+// let a=10
+// let b=5
+// console.log(a+=b);
+// console.log(a-=b);
+// console.log(a*=b);
+// console.log(a/=b);
+// console.log(a%=b);
+// logical opertaor
+// let a:boolean=true
+// let b:boolean=false
+// console.log(a&&b);
+// console.log(a||b);
+// console.log(!a);
+// let a=10
+// let b=5
+// console.log(a>b);//t
+// console.log(a<b);//f
+// console.log(a<=b);//f
+// console.log(a>=b);//t
+// console.log(a!=b);//t
+// console.log(b!=a);//t
+// console.log(a==b);//f
+/*loops*/
+// while loop
+// let a=1
+// while(a<10){
+//     console.log(a);
+//     a++;
+// }
+//do while
+// let a=1
+// do{
+//     console.log(a);
+//     a++;
+// }
+// while(a<10);
+// for loop
+// for(let i=1;i<10;i++){
+//     console.log('umar');
+// }
+// for in loop
+// let i:any
+// let n:any=["a b c"]
+// for(i in n){
+//     console.log(n[i]); 
+// }
+// decision making
+// if state
+// let a=10
+// let b=5
+// if(a<b){
+//     console.log(true);
+// }
+// if else state
+// let a=10
+// let b=5
+// if(a<b){
+//     console.log(false);
+// }else{
+//     console.log(true);
+// }
+// if else if
+// let a=1
+// let b=2
+// let c=3
+// if(a>b){
+//     console.log(a);
+// }else if(a>c){
+//     console.log(b);
+// }else{
+//     console.log(c);
+// }
+// // switch state
+// let grade:string="F"
+// switch(grade){
+//     case "A":{
+//         console.log("first");
+//         break;
+//     }
+//     case "B": {
+//         console.log("2nd");
+//         break;
+//     }
+//     default:{
+//         console.log("legend");
+//          break;
+//     }
+// }
+// let umar=function(a:number,b:string){
+//     return a+""+b
+// }
+// console.log(umar(10,"umar"));
+var a = ['ali', 'umar', 123];
+a.push(123);
+console.log(a);
